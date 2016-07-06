dd46bdcc05cbba617fdbc4f12cf2ce568dc0a430
James Dabbs <jamesdabbs@gmail.com>
2016-07-06 10:52:57 -0400

    Working Slack slash command

--- app/controllers/hangman_controller.rb
+++ app/controllers/hangman_controller.rb
@@ -1,4 +1,6 @@
 class HangmanController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:slack]
+
   def show
     @game = Hangman.where(player: current_user).find params[:id]
   end
@@ -11,5 +13,20 @@ class HangmanController < ApplicationController
       f.html { redirect_to :back }
       f.json { render :show }
     end
+  end
+
+  def slack
+    user_from_slack = User.first
+
+    if params[:text] == "start"
+      game = Hangman.start user_from_slack
+    else
+      letter = params[:text]
+      game   = Hangman.where(player: user_from_slack).last
+      game.record_guess letter
+    end
+
+    board = game.board.join(" ")
+    render text: "`#{board}`"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   post "/start" => "pages#start", as: :start_game
+  post "/slack/hangman" => "hangman#slack"
 
   resource :games, only: [] do
     resources :hangman, except: [:destroy]
